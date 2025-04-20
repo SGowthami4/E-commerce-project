4a9a9a60ecabc22eb55c88d037cc0a4bb0f08fbe
SGowthami4 <gowthamigowthami686@gmail.com>
2025-04-20 18:34:22 +0530

    updated config file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/", // Keep it as "/" if your app is at the root of your website
+  build: {
+    sourcemap: false, // Disable source maps in production
+  },
+});
